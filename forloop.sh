40a5e0dd44294b5814ac156df96a2d786b0ba9af
ashadeep <ashadeep746@gmail.com>
2023-01-23 09:36:57 +0530

    main

---
+++ forloop.sh
@@ -0,0 +1,6 @@
+read number1
+for(( i=1 ; i<=number1; i++))
+do
+echo $number1
+echo $i
+done
---
+++ forloop1.sh
@@ -0,0 +1,13 @@
+read number1;
+for((i=1 ; i<=number1; i++))
+do
+if((number1%2!=0))
+echo $number1
+else
+echo "no number"
+fi
+done
+
+
+
+
